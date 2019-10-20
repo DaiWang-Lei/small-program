91935ce2ed378b1b9626f0dad7e50bb0a106c4d5
代罔 <1403693160@qq.com>
2019-10-20 08:45:47 +0800

    first commitbilibili小程序

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg: [{
+        src: "http://b159.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/XYQaFKRrGEz3cu1mijIidTb5NYnA8Duie49dYPmTW*U!/m/dJ8AAAAAAAAAnull&bo=WwhABsAPyAsRCSc!&rf=photolist&t=5"
+      }, {
+        src: "http://b154.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/r3aAeAgegssfp7CrL2hjw6nGJipaRvZuuEwk4PX06Cg!/m/dJoAAAAAAAAAnull&bo=WwhABsAPyAsRCSc!&rf=photolist&t=5"
+      },
+      {
+        src: "http://b152.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/*DPIW5yPRC7M28WXV0ZJ4N9.n8l7Gi8h2nsjQ3wdKUg!/m/dJgAAAAAAAAAnull&bo=VQhABgASgA0RCbo!&rf=photolist&t=5"
+      }
+    ],
+    newsList: [{
+        id: "356412",
+        title: "天安门城楼前的我们，第一次去那么远的地方",
+        poster: "http://b154.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/r3aAeAgegssfp7CrL2hjw6nGJipaRvZuuEwk4PX06Cg!/m/dJoAAAAAAAAAnull&bo=WwhABsAPyAsRCSc!&rf=photolist&t=5",
+        add_date: "2018-09-29"
+      },
+      {
+        id: "546734",
+        title: "颐和园美妙的旅程,数不尽的怀念",
+        poster: "http://b289.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/evfx6rEohpZ7xqY94EhU00z1Le8B8qCve6ZF5iprsx4!/m/dCEBAAAAAAAAnull&bo=WwhABsAPyAsRCSc!&rf=photolist&t=5",
+        add_date: "2018-09-30"
+      },
+      {
+        id: "239875",
+        title: "那时的情形，我现在还记得，一切都那么美丽，明亮",
+        poster: "http://b289.photo.store.qq.com/psb?/sa14V11H6BCm1YsGAw32cd1f3b90a936385153692d4e6ac420d314V142RH3z3rCXok/m0yPcJxSQcvm81S37mMGDwzRoRl39ujLmtJQr3k*ya4!/m/dCEBAAAAAAAAnull&bo=QAZbCMAPyAsRCSc!&rf=photolist&t=5",
+        add_date: "2018-09-30"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
